b9bcc84bcad82f0006b68d543c6853e41b0cf5e1
Craig Morrison <morrisontech@yahoo.com>
2016-04-01 23:43:03 -0700

    commit

--- app/src/main/java/rocks/morrisontech/seattletrailsmap/OffLeashParks.java
+++ app/src/main/java/rocks/morrisontech/seattletrailsmap/OffLeashParks.java
@@ -1,5 +1,8 @@
 package rocks.morrisontech.seattletrailsmap;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,24 +17,34 @@ import javax.net.ssl.HttpsURLConnection;
 public class OffLeashParks {
 
     //Connect to server and get data
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception, JSONException {
 
+        //variables used to parse Json data
+        double latitude, longitude;
+        String parkName;
+
+        //socrata connection data
         String socAppToken = "ZSFz0bJfTTnGi8aQxJJLQ9TuB"; //find where to pass this to Socrata for authorization
         String httpsURL = "https://data.seattle.gov/resource/ybmn-w2mc.json";
 
+        //open connection
         URL offLeashURL = new URL(httpsURL);
         HttpsURLConnection con = (HttpsURLConnection) offLeashURL.openConnection();
         InputStream ins = con.getInputStream();
-        InputStreamReader isr = new InputStreamReader(ins);
+        InputStreamReader isr = new InputStreamReader(ins, "UTF-8");
         BufferedReader in = new BufferedReader(isr);
 
+        //string variable hold each record as it is read
         String inputLine;
 
+        JSONObject object;
         /*
         Create ArrayList of off-leash parks with data from Socrata
         ArrayList allows flexibility of size without reprogramming when the number of objects changes
          */
         ArrayList<OffLeashData> parkData = new ArrayList<>();
+
+        int index = 0;
 
 
         while ((inputLine = in.readLine()) != null) {
@@ -39,7 +52,8 @@ public class OffLeashParks {
             //write each line into an OffLeashData object
             System.out.println(inputLine);
 
-            //parse data from connection into OffLeashData objects
+            //parse data from connection into OffLeashData objects (name, latitude, longitude)
+
             }
 
         //close data connection once all park objects are instantiated
@@ -64,6 +78,11 @@ public class OffLeashParks {
     {
         String parkName;
         double latitude, longitude;
+    }
+
+    public static void placePins()
+    {
+        //use this method to place pins on map where dog parks are
     }
 
 }
